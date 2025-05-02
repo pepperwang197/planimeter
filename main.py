dfa3753ec4124538e5961a18f0b3f6f1053540c6
Pepper Wang <pwang@olin.edu>
2025-05-01 21:19:26 -0400

    labels and grid

--- main.py
+++ main.py
@@ -13,59 +13,111 @@ def find_intersection(x1, y1, x2, y2, r1, r2):
     f = sympy.Poly((x - x2) ** 2 + (y - y2) ** 2 - r2**2)
     x_vals = sympy.nroots(f)
 
-    x_pivot = float(x_vals[0])
+    try:
+        x_pivot = float(x_vals[0])
 
-    y = sympy.Symbol("y")
-    y_vals_circle1 = sympy.nroots(
-        sympy.Poly((y - y2) ** 2 + (x_pivot - x2) ** 2 - r2**2)
-    )
-    y_vals_circle2 = sympy.nroots(
-        sympy.Poly((y - y1) ** 2 + (x_pivot - x1) ** 2 - r1**2)
-    )
+        y = sympy.Symbol("y")
+        y_vals_circle1 = sympy.nroots(
+            sympy.Poly((y - y2) ** 2 + (x_pivot - x2) ** 2 - r2**2)
+        )
+        y_vals_circle2 = sympy.nroots(
+            sympy.Poly((y - y1) ** 2 + (x_pivot - x1) ** 2 - r1**2)
+        )
 
-    y_vals_circle1_int = {int(num) for num in y_vals_circle1}
-    y_vals_circle2_int = {int(num) for num in y_vals_circle2}
+        y_vals_circle1_int = {int(num) for num in y_vals_circle1}
+        y_vals_circle2_int = {int(num) for num in y_vals_circle2}
 
-    print("circle 1", y_vals_circle1_int)
-    print("circle 2", y_vals_circle2_int)
+        return (int(x_pivot), list(y_vals_circle1_int & y_vals_circle2_int)[0])
 
-    return (int(x_pivot), list(y_vals_circle1_int & y_vals_circle2_int)[0])
+    except (TypeError, IndexError):
+        return None
+
+
+def display_text(scrn, text, color, coords, x_offset=10, y_offset=25):
+    pygame.font.init()
+    font = pygame.font.Font("freesansbold.ttf", 32)
+    text = font.render(text, True, color)
+    text_rect = text.get_rect()
+    text_rect.center = (coords[0] + x_offset, coords[1] + y_offset)
+    scrn.blit(text, text_rect)
+
+
+def draw_grid(scrn, xmax, ymax, interval=20):
+    for i in range(0, xmax, interval):
+        pygame.draw.line(scrn, pygame.Color("#bfbfbf"), (i, 0), (i, ymax))
+
+    for i in range(0, ymax, interval):
+        pygame.draw.line(scrn, pygame.Color("#bfbfbf"), (0, i), (xmax, i))
 
 
 def main():
 
-    scrn = pygame.display.set_mode((600, 600))
-    pygame.display.set_caption("image")
+    xmax = 500
+    ymax = 500
 
-    P = 250
-    T = 250
-    CENTER = (300, 300)
+    scrn = pygame.display.set_mode((xmax, ymax))
+    pygame.display.set_caption("Planimeter simulation")
 
-    xy = (0, 0)
-    pivot = (0, 0)
+    P = 300
+    T = 300
+    CENTER = (100, 100)
 
     all_coords = []
+    # wheel_positions = []  ############
+
+    light_blue = pygame.Color("#1d88c2")
+    magenta = pygame.Color("#b33484")
+
+    scrn.fill("white")
+    xy = (400, 200)
+    pivot = find_intersection(CENTER[0], CENTER[1], xy[0], xy[1], T, P)
+    draw_grid(scrn, xmax, ymax)
+    pygame.draw.line(scrn, "blue", CENTER, pivot, width=10)
+    pygame.draw.line(scrn, "red", xy, pivot, width=10)
+    pygame.draw.circle(scrn, light_blue, pivot, 8)
+    pygame.draw.circle(scrn, magenta, xy, 8)
+    pygame.draw.circle(scrn, "black", CENTER, 8)
+    display_text(scrn, "(a,b)", light_blue, pivot)
+    display_text(scrn, "(x,y)", magenta, xy)
+    display_text(scrn, "(0,0)", "black", CENTER, x_offset=-20, y_offset=-25)
+    pygame.display.flip()
 
     while True:
-        scrn.fill("black")
+        scrn.fill("white")
+        draw_grid(scrn, xmax, ymax)
 
         # get user input for current position
         pygame.event.get()
         if pygame.mouse.get_pressed()[0]:
             xy = pygame.mouse.get_pos()
-            all_coords.append(xy)
             # calculate pivot point
             pivot = find_intersection(CENTER[0], CENTER[1], xy[0], xy[1], T, P)
-            print(pivot)
+            if pivot is not None:
+                all_coords.append(xy)
 
-        if len(all_coords) >= 2:
-            pygame.draw.lines(scrn, "white", False, all_coords)
+        if pivot is not None:
+            if len(all_coords) >= 2:
+                pygame.draw.lines(scrn, "black", False, all_coords, width=2)
 
-        if pivot[0] != 0 and pivot[1] != 0:
+            # wheel = (
+            #     xy[0] + (pivot[0] - xy[0]) / 2,
+            #     xy[1] + (pivot[1] - xy[1]) / 2,
+            # )  ###############
+            # wheel_positions.append(wheel)  ###########
+            # if len(wheel_positions) >= 2:
+            #     pygame.draw.lines(
+            #         scrn, "orange", False, wheel_positions, width=2
+            #     )  ###############
+
             pygame.draw.line(scrn, "blue", CENTER, pivot, width=10)
             pygame.draw.line(scrn, "red", xy, pivot, width=10)
-
-        pygame.display.flip()
+            pygame.draw.circle(scrn, light_blue, pivot, 8)
+            pygame.draw.circle(scrn, magenta, xy, 8)
+            pygame.draw.circle(scrn, "black", CENTER, 8)
+            display_text(scrn, "(a,b)", light_blue, pivot)
+            display_text(scrn, "(x,y)", magenta, xy)
+            display_text(scrn, "(0,0)", "black", CENTER, x_offset=-20, y_offset=-25)
+            pygame.display.flip()
         sleep(0.002)
 
 
